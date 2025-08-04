59a6d86c41c9a1f1fee1e6dcfb1a6b645c2b2904
Ravishankar-511 <ravishankarrachuri@gmail.com>
2025-08-04 15:08:18 +0530

    I am commiting

---
+++ project01/src/myapp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Myapp extends Component {
+    render() {
+        return (
+            <div>
+                KL university
+            </div>
+        );
+    }
+}
+
+export default Myapp;
