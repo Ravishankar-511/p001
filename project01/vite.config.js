de06a991e8d890c05bc68fbfe97118107cf01a74
Ravishankar-511 <ravishankarrachuri@gmail.com>
2025-08-06 08:03:56 +0530

    Prepare for deployment

--- project01/vite.config.js
+++ project01/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 1234}
+  server: {port: 1006},
+  base:'/Project01/'
 })
